74531badfe9aeabd5cfb85e605111e946d8e6666
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-01-15 14:45:28 -0300

    feat: :construction: Botão para fechar a tela de resultado do IMC funcionando

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ const form = document.querySelector('form');
 const inputPeso = document.querySelector('#peso');
 const inputAltura = document.querySelector('#altura');
 
-form.onsubmit = function(e) {
-    e.preventDefault();
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+form.onsubmit = event => {
+    event.preventDefault();
     
     const peso = inputPeso.value;
     const altura = inputAltura.value;
 
-    const result = IMC(peso, altura)
-    console.log(result)
+    const result = IMC(peso, altura);
+    console.log(result);
+
+    const message = `Seu IMC é de ${result}`;
+
+    modalMessage.innerText = message;
+    modalWrapper.classList.add('open');
 };
+
+modalBtnClose.onclick = () => modalWrapper.classList.remove('open');
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
