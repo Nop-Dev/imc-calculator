70699a9b44c9bbcbcc247714a9ff04f14f2b8648
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-01-19 22:15:04 -0300

    feat: :construction: Tecla esc para fechar o modal implementada...

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-
 // Variáveis
+import { Modal } from './modal.js'
 const form = document.querySelector('form');
 
 const inputPeso = document.querySelector('#peso');
 const inputAltura = document.querySelector('#altura');
-
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
 
 form.onsubmit = event => {
     event.preventDefault();
@@ -20,11 +16,9 @@ form.onsubmit = event => {
 
     const message = `Seu IMC é de ${result}`;
 
-    modalMessage.innerText = message;
-    modalWrapper.classList.add('open');
+    Modal.message.innerText = message;
+    Modal.open();
 };
-
-modalBtnClose.onclick = () => modalWrapper.classList.remove('open');
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
